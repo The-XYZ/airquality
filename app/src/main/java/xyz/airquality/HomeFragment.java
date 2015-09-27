e3afca640279628f83b7fcf12b94e7058ef32844
yogeshbalan <yogeshbalan@live.co.uk>
2015-09-27 14:36:00 +0530

    Damn imls
    Signed-off-by: yogeshbalan <yogeshbalan@live.co.uk>
    
    Signed-off-by: yogeshbalan <yogeshbalan@live.co.uk>
    
    Signed-off-by: yogeshbalan <yogeshbalan@live.co.uk>

--- app/src/main/java/xyz/airquality/HomeFragment.java
+++ app/src/main/java/xyz/airquality/HomeFragment.java
@@ -4,15 +4,27 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 /**
  * Created by naman on 27/09/15.
  */
 public class HomeFragment extends Fragment {
+
+    private GoogleMap mMap;
+    private RecyclerView mRecyclerView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -23,11 +35,40 @@ public class HomeFragment extends Fragment {
         ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
 
         final ActionBar ab = ((AppCompatActivity) getActivity()).getSupportActionBar();
-        ab.setHomeAsUpIndicator(R.drawable.ic_menu);
+        ab.setHomeAsUpIndicator(R.drawable.menu);
         ab.setDisplayHomeAsUpEnabled(true);
+
+        mRecyclerView=(RecyclerView) rootView.findViewById(R.id.recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL,false));
+        mRecyclerView.setHasFixedSize(true);
+
+        addToMap("78.98,78.98","title");
 
         return rootView;
 
+
+    }
+
+    private void addToMap(String latlong,String title){
+        mMap = ((SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map)).getMap();
+
+
+        MarkerOptions markerOptions;
+        LatLng position;
+        String lati=latlong.substring(0,latlong.indexOf(",")),longi=latlong.substring(latlong.indexOf(",")+1,latlong.length());
+
+        markerOptions = new MarkerOptions();
+
+
+        position = new LatLng(Double.parseDouble(lati), Double.parseDouble(longi));
+        markerOptions.position(position);
+        markerOptions.title(title);
+        mMap.addMarker(markerOptions);
+
+        CameraUpdate cameraPosition = CameraUpdateFactory.newLatLngZoom(position, 6.0f);
+
+
+        mMap.animateCamera(cameraPosition);
 
     }
 }
