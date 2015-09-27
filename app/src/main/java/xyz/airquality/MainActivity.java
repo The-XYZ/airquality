83e16b8917c72721fb34385b8dee61b7ddb56497
naman14 <namandwivedi14@gmail.com>
2015-09-27 13:47:33 +0530

    nav drawer

--- app/src/main/java/xyz/airquality/MainActivity.java
+++ app/src/main/java/xyz/airquality/MainActivity.java
@@ -1,5 +1,12 @@
 package xyz.airquality;
 
+import android.content.Intent;
+import android.os.Handler;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -7,31 +14,77 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DrawerLayout mDrawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        if (navigationView != null) {
+            setupDrawerContent(navigationView);
+        }
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        final android.support.v4.app.FragmentTransaction transaction = fragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, new HomeFragment());
+        transaction.commit();
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        updatePosition(menuItem);
+                        return true;
+                    }
+                });
+    }
+
+    private void updatePosition(final MenuItem menuItem) {
+        Fragment fragment = null;
+        Intent intent = null;
+        switch (menuItem.getItemId()) {
+            case R.id.nav_home:
+                fragment = new HomeFragment();
+                break;
         }
 
-        return super.onOptionsItemSelected(item);
+        if (fragment != null) {
+            menuItem.setChecked(true);
+            mDrawerLayout.closeDrawers();
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            final android.support.v4.app.FragmentTransaction transaction = fragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, fragment);
+            Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    transaction.commit();
+                }
+            }, 350);
+        }
     }
 }
