d21bbc83a4ca74cd2e54b117382b1f5993acbccc
naman14 <namandwivedi14@gmail.com>
2015-09-27 19:12:42 +0530

    move map to position

--- app/src/main/java/xyz/airquality/HomeFragment.java
+++ app/src/main/java/xyz/airquality/HomeFragment.java
@@ -7,7 +7,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,6 +23,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.TileOverlayOptions;
 import com.google.maps.android.heatmaps.HeatmapTileProvider;
 import com.google.maps.android.heatmaps.WeightedLatLng;
+import com.google.maps.android.ui.IconGenerator;
 import com.parse.FindCallback;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
@@ -55,16 +55,27 @@ public class HomeFragment extends Fragment {
 
         mMap = ((SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map)).getMap();
 
-        spinner=(Spinner)rootView.findViewById(R.id.spinner_nav);
+        spinner = (Spinner) rootView.findViewById(R.id.spinner_nav);
         spinner.setVisibility(View.VISIBLE);
         final List<String> list = new ArrayList<String>();
         list.add("DELHI");
         list.add("GUJARAT");
         list.add("ARUNACHAL PRADESH");
         list.add("PUDUCHERRY");
-        list.add("JHARKHAND");list.add("HARYANA");list.add("MANIPUR");list.add("GOA");list.add("MEGHALAYA");
-        list.add("CHHATTISGARH");list.add("LAKSHADWEEP");list.add("KERALA");list.add("TAMIL NADU");
-        list.add("RAJASTHAN");list.add("DELHI");list.add("UTTAR PRADESH");list.add("NAGALAND");list.add("MAHARASHTRA");
+        list.add("JHARKHAND");
+        list.add("HARYANA");
+        list.add("MANIPUR");
+        list.add("GOA");
+        list.add("MEGHALAYA");
+        list.add("CHHATTISGARH");
+        list.add("LAKSHADWEEP");
+        list.add("KERALA");
+        list.add("TAMIL NADU");
+        list.add("RAJASTHAN");
+        list.add("DELHI");
+        list.add("UTTAR PRADESH");
+        list.add("NAGALAND");
+        list.add("MAHARASHTRA");
 
         ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(getActivity(),
                 android.R.layout.simple_spinner_item, list);
@@ -85,8 +96,8 @@ public class HomeFragment extends Fragment {
             }
         });
 
-        mRecyclerView=(RecyclerView) rootView.findViewById(R.id.recycler_view);
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL,false));
+        mRecyclerView = (RecyclerView) rootView.findViewById(R.id.recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false));
         mRecyclerView.setHasFixedSize(true);
 
         stationsAdapter = new StationsAdapter(getActivity(), new ArrayList<ParseObject>());
@@ -101,11 +112,10 @@ public class HomeFragment extends Fragment {
 
         ParseQuery query = ParseQuery.getQuery("StateRemark");
         query.whereEqualTo("State", state);
-        Log.d("lol",state);
         query.findInBackground(new FindCallback<ParseObject>() {
             @Override
             public void done(final List<ParseObject> objects, com.parse.ParseException e) {
-                if (e==null) {
+                if (e == null) {
                     stationsAdapter.updateDataset(objects);
                     createHeatMap(state, objects);
                 } else {
@@ -115,23 +125,39 @@ public class HomeFragment extends Fragment {
         });
     }
 
-    private void addToMap(String latlong,String title){
+    private void addToMap(LatLng position, String title, int pollutionLevel) {
+
+        IconGenerator ig = new IconGenerator(getActivity());
+
+        switch (pollutionLevel) {
+            case 1:
+                ig.setStyle(IconGenerator.STYLE_GREEN);
+                break;
+            case 2:
+                ig.setStyle(IconGenerator.STYLE_ORANGE);
+                break;
+            case 3:
+                ig.setStyle(IconGenerator.STYLE_PURPLE);
+                break;
+            case 4:
+                ig.setStyle(IconGenerator.STYLE_RED);
+                break;
+            default:
+                ig.setStyle(IconGenerator.STYLE_RED);
+                break;
+
+        }
 
         MarkerOptions markerOptions;
-        LatLng position;
-        String lati=latlong.substring(0,latlong.indexOf(",")),longi=latlong.substring(latlong.indexOf(",")+1,latlong.length());
-
         markerOptions = new MarkerOptions();
 
-        position = new LatLng(Double.parseDouble(lati), Double.parseDouble(longi));
         markerOptions.position(position);
         markerOptions.title(title);
+//        markerOptions.icon(BitmapDescriptorFactory
+//                .fromBitmap(ig.makeIcon(String.valueOf(pollutionLevel))));
         mMap.addMarker(markerOptions);
 
-        CameraUpdate cameraPosition = CameraUpdateFactory.newLatLngZoom(position, 6.0f);
-
-
-        mMap.animateCamera(cameraPosition);
+//        moveMapToPosition(6f, position);
 
     }
 
@@ -139,7 +165,8 @@ public class HomeFragment extends Fragment {
     private void createHeatMap(String state, List<ParseObject> stations) {
         List<StatePollutionObject> states = new ArrayList<>();
         for (ParseObject object : stations) {
-            states.add(new StatePollutionObject(state,object.getParseGeoPoint("latlon2").getLatitude(),object.getParseGeoPoint("latlon2").getLongitude(),object.getNumber("Remark").intValue()));
+            states.add(new StatePollutionObject(state, object.getParseGeoPoint("latlon2").getLatitude(), object.getParseGeoPoint("latlon2").getLongitude(), object.getNumber("Remark").intValue()));
+            addToMap(new LatLng(object.getParseGeoPoint("latlon2").getLatitude(),object.getParseGeoPoint("latlon2").getLongitude()),object.getString("Station"),object.getNumber("Remark").intValue());
         }
 
         List<WeightedLatLng> pointList = new ArrayList<WeightedLatLng>();
@@ -153,9 +180,18 @@ public class HomeFragment extends Fragment {
         if (!pointList.isEmpty()) {
             HeatmapTileProvider provider =
                     new HeatmapTileProvider.Builder()
-                            .weightedData(pointList) //FIXME: this comes to be null sometimes
+                            .weightedData(pointList)
+                                    .radius(50)
                             .build();
             mMap.addTileOverlay(new TileOverlayOptions().tileProvider(provider));
         }
+
+        moveMapToPosition(10f, new LatLng(stations.get(0).getParseGeoPoint("latlon2").getLatitude(), stations.get(0).getParseGeoPoint("latlon2").getLongitude()));
+    }
+
+    private void moveMapToPosition(float ratio, LatLng position) {
+        CameraUpdate cameraPosition = CameraUpdateFactory.newLatLngZoom(position, ratio);
+        mMap.animateCamera(cameraPosition);
+
     }
 }
